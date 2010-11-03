0cacdd4756aa17787f6e7f12c4f8025ff020d1c1
Eric Moritz <eric@themoritzfamily.com>
2010-11-03 15:45:00 +0000

    Added my custom stuff

---
+++ custom.el
@@ -0,0 +1,37 @@
+;(global-font-lock-mode 1)
+;(setq default-tab-width 4)
+;(setq-default indent-tabs-mode nil)
+;(put 'narrow-to-region 'disabled nil)
+
+;disable backup.
+(setq backup-inhibited t)
+
+
+;(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+;(add-hook 'py-shell-hook 'ansi-color-for-comint-mode-on)
+
+;; Load our custom libraries
+(add-to-list 'load-path "~/.emacs.d/site-packages")
+;(add-to-list 'load-path "~/.emacs.d/site-packages/org-mode")
+
+(load "80-split.el")
+(load "ideal-python-environment.el")
+;(load "go-mode-load.el")
+;(load "org-mode-load.el")
+
+;; Window Movement Keys
+(global-set-key "\C-c\C-p" 'windmove-up)
+(global-set-key "\C-c\C-f" 'windmove-right)
+(global-set-key "\C-c\C-n" 'windmove-down)
+(global-set-key "\C-c\C-b" 'windmove-left)
+(global-set-key "\C-c\C-b" 'windmove-left)
+(global-set-key "\M-g\M-g" 'goto-line)
+
+;; Always open the inital window at 79 chars
+(do-80-split)
+
+;; Turn on truncated lines
+(setq-default truncate-lines t)
+
+(add-hook 'python-mode-hook
+          (lambda () (flyspell-prog-mode)))
